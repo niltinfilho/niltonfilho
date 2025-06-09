adc3807e21fcb3b3e736fea94204a0a9a72dd7a6
nilton <niltonfilho113@gmail.com>
2025-06-09 02:28:32 -0300

    criando os artigos

---
+++ teste.js
@@ -0,0 +1,5 @@
+function sendWhatsapp() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+}
