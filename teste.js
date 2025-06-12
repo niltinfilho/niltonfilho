854f36f31cb205b0b0c95af5ab5377f464e51a83
nilton <niltonfilho113@gmail.com>
2025-06-12 10:00:46 -0300

    finalizando o projeto

--- teste.js
+++
@@ -1,5 +0,0 @@
-function sendWhatsapp() {
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let message = document.getElementById("message").value;
-}
